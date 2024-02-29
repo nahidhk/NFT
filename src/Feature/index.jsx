8886bc7333d055632f352292dd9697fdc592c856
soum-ik <sarkarsoumik215@gmail.com>
2024-02-29 21:51:03 +0600

    project done

--- src/Feature/index.jsx
+++ src/Feature/index.jsx
@@ -1,4 +1,7 @@
 import HeadingLayout from "../components/HeadingLayout";
+import card1 from "../../public/card.png";
+import card2 from "../../public/card2.png";
+import card3 from "../../public/card3.png";
 
 const data = {
   title: "Our  Feature",
@@ -10,153 +13,58 @@ export default function Feature() {
   return (
     <div>
       <HeadingLayout data={data} />
-      <main className="max-w-6xl mx-auto pt-10 pb-36 px-8">
-        <div className="flex flex-col justify-between items-center lg:flex-row lg:items-start">
-          <div className="w-full flex-1 mt-8 p-8 order-2 bg-white shadow-xl rounded-3xl sm:w-96 lg:w-full lg:order-1 lg:rounded-r-none">
-            <div className="mb-7 pb-7 flex items-center border-b border-gray-300">
-              <img
-                src="https://res.cloudinary.com/williamsondesign/abstract-1.jpg"
-                alt=""
-                className="rounded-3xl w-20 h-20"
-              />
-              <div className="ml-5">
-                <span className="block text-2xl font-semibold">Basic</span>
-                <span>
-                  <span className="font-medium text-gray-500 text-xl align-top"></span>
-                  <span className="text-3xl font-bold">10 </span>
-                </span>
-                <span className="text-gray-500 font-medium">/ user</span>
-              </div>
+      <main className="max-w-6xl mx-auto pt-10 pb-36 px-8 relative">
+        <div className="flex flex-col  justify-between items-center lg:flex-row">
+          {/*  */}
+          <div className="w-full flex-1 mt-8 p-8 order-2 bg-[background: rgba(0, 25, 34, 1)] shadow-xl rounded-3xl sm:w-96 lg:w-full  lg:rounded-r-none">
+            <div className="     flex items-center justify-center">
+              <img src={card2} alt="" className="rounded-3xl size-2/4" />
             </div>
-            <ul className="mb-7 font-medium text-gray-500">
-              <li className="flex text-lg mb-2">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  Get started with <span className="text-black">messaging</span>
-                </span>
-              </li>
-              <li className="flex text-lg mb-2">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  Flexible <span className="text-black">team meetings</span>
-                </span>
-              </li>
-              <li className="flex text-lg">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  <span className="text-black">5 TB</span> cloud storage
-                </span>
-              </li>
-            </ul>
-            <a
-              href="#/"
-              className="flex justify-center items-center bg-indigo-600 rounded-xl py-5 px-4 text-center text-white text-xl"
-            >
-              Choose Plan
-              <img
-                src="https://res.cloudinary.com/williamsondesign/arrow-right.svg"
-                className="ml-2"
-              />
-            </a>
+            <div className="  space-y-2 md:space-y-3 mb-10 md:mb-0 flex flex-col place-content-center place-items-center">
+              <h1 className=" text-color text-[30px] md:text-[38px] lg:text-[45px]">
+                Discover
+              </h1>
+              <h1 className=" text-[15px] text-color opacity-65 md:text-[18px] lg:text-[24px]  text-center">
+                A cryptocurrency can be acquired through so called “mining”.
+                Mining is conducted by the verification.
+              </h1>
+              <h2 className=" text-[24px]  text-color-v3">Learn more--&gt;</h2>
+            </div>
           </div>
-          <div className="w-full flex-1 p-8 order-1 shadow-xl rounded-3xl bg-gray-900 text-gray-400 sm:w-96 lg:w-full lg:order-2 lg:mt-0">
-            <div className="mb-8 pb-8 flex items-center border-b border-gray-600">
-              <img
-                src="https://res.cloudinary.com/williamsondesign/abstract-2.jpg"
-                alt=""
-                className="rounded-3xl w-20 h-20"
-              />
-              <div className="ml-5">
-                <span className="block text-3xl font-semibold text-white">
-                  Startup
-                </span>
-                <span>
-                  <span className="font-medium text-xl align-top"> </span>
-                  <span className="text-3xl font-bold text-white">24 </span>
-                </span>
-                <span className="font-medium">/ user</span>
-              </div>
+          {/*  */}
+          <div className="w-full flex-1 mt-8 p-8 order-2 bg-[background: rgba(0, 25, 34, 1)] shadow-xl rounded-3xl sm:w-96 lg:w-full  lg:rounded-r-none">
+            <div className="  lg:my-3   flex items-center justify-center">
+              <img src={card1} alt="" className="rounded-3xl lg:size-3/4" />
             </div>
-            <ul className="mb-10 font-medium text-xl">
-              <li className="flex mb-6">
-                <img src="https://res.cloudinary.com/williamsondesign/check-white.svg" />
-                <span className="ml-3">
-                  All features in <span className="text-white">Basic</span>
-                </span>
-              </li>
-              <li className="flex mb-6">
-                <img src="https://res.cloudinary.com/williamsondesign/check-white.svg" />
-                <span className="ml-3">
-                  Flexible <span className="text-white">call scheduling</span>
-                </span>
-              </li>
-              <li className="flex">
-                <img src="https://res.cloudinary.com/williamsondesign/check-white.svg" />
-                <span className="ml-3">
-                  <span className="text-white">15 TB</span> cloud storage
-                </span>
-              </li>
-            </ul>
-            <a
-              href="#/"
-              className="flex justify-center items-center bg-indigo-600 rounded-xl py-6 px-4 text-center text-white text-2xl"
-            >
-              Choose Plan
-              <img
-                src="https://res.cloudinary.com/williamsondesign/arrow-right.svg"
-                className="ml-2"
-              />
-            </a>
+            <div className="  space-y-2 md:space-y-3 mb-10 md:mb-0 flex flex-col place-content-center place-items-center">
+              <h1 className=" text-color text-[30px] md:text-[38px] lg:text-[45px]">
+                Discover
+              </h1>
+              <h1 className=" text-[15px] text-color opacity-65 md:text-[18px] lg:text-[24px]  text-center">
+                A cryptocurrency can be acquired through so called “mining”.
+                Mining is conducted by the verification.
+              </h1>
+              <h2 className=" text-[24px]  text-color-v3">Learn more--&gt;</h2>
+            </div>
           </div>
-          <div className="w-full flex-1 mt-8 p-8 order-3 bg-white shadow-xl rounded-3xl sm:w-96 lg:w-full lg:order-3 lg:rounded-l-none">
-            <div className="mb-7 pb-7 flex items-center border-b border-gray-300">
-              <img
-                src="https://res.cloudinary.com/williamsondesign/abstract-3.jpg"
-                alt=""
-                className="rounded-3xl w-20 h-20"
-              />
-              <div className="ml-5">
-                <span className="block text-2xl font-semibold">Enterprise</span>
-                <span>
-                  <span className="font-medium text-gray-500 text-xl align-top"></span>
-                  <span className="text-3xl font-bold">35 </span>
-                </span>
-                <span className="text-gray-500 font-medium">/ user</span>
-              </div>
+          {/* start */}
+          <div className="w-full flex-1 mt-8 p-8 order-2 bg-[background: rgba(0, 25, 34, 1)] shadow-xl rounded-3xl sm:w-96 lg:w-full  lg:rounded-r-none">
+            <div className="     flex items-center justify-center">
+              <img src={card3} alt="" className="rounded-3xl " />
             </div>
-            <ul className="mb-7 font-medium text-gray-500">
-              <li className="flex text-lg mb-2">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  All features in <span className="text-black">Startup</span>
-                </span>
-              </li>
-              <li className="flex text-lg mb-2">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  Growth <span className="text-black">oriented</span>
-                </span>
-              </li>
-              <li className="flex text-lg">
-                <img src="https://res.cloudinary.com/williamsondesign/check-grey.svg" />
-                <span className="ml-3">
-                  <span className="text-black">Unlimited</span> cloud storage
-                </span>
-              </li>
-            </ul>
-            <a
-              href="#/"
-              className="flex justify-center items-center bg-indigo-600 rounded-xl py-5 px-4 text-center text-white text-xl"
-            >
-              Choose Plan
-              <img
-                src="https://res.cloudinary.com/williamsondesign/arrow-right.svg"
-                className="ml-2"
-              />
-            </a>
+            <div className="  space-y-2 md:space-y-3 mb-10 md:mb-0 flex flex-col place-content-center place-items-center">
+              <h1 className=" text-color text-[30px] md:text-[38px] lg:text-[45px]">
+                Launchpad
+              </h1>
+              <h1 className=" text-[15px] text-color opacity-65 md:text-[18px] lg:text-[24px] font-light    text-center">
+                Crypto supports the blockchain ecosystem of various projects and
+                invites thee creators from project.
+              </h1>
+              <h2 className=" text-[24px]  text-color-v3">Learn more--&gt;</h2>
+            </div>
           </div>
         </div>
-      </main>
+      </main> 
     </div>
   );
 }
--- src/comunity/index.jsx
+++ src/comunity/index.jsx
@@ -1,17 +1,17 @@
 export default function Feature() {
   return (
     <div className=" container flex flex-col flex-1 text-color items-center justify-center">
-      <h1 className="  text-[40px] text-center font-semibold leading-[43px]">
+      <h1 className="  text-[32px] lg:text-[40px] text-center min-w-[350px] font-semibold leading-[43px]">
         {" "}
         Join Our Community And get Cryptos and <br /> discover unique places
       </h1>
-      <h2 className=" opacity-80 text-[24px] mt-5 leading-[36px] text-center">
-        Join our presale of only1000 censored pigs .Presale <br /> live on
+      <h2 className=" opacity-80 lg:text-[24px] text-[18px] mt-5 lg:leading-[36px] leading-[29px] text-center">
+        Join our presale of only1000 censored pigs .Presale   live on
         february 28th at 8:00 pm utc.
       </h2>
-      <button className="px-4 py-2 mt-6 rounded-ss-xl rounded-ee-xl font-medium text-[18px] gradient-background hover:text-white/80  text-neutral-900">
+      <button className=" text-color-v3 px-4 py-2 mt-6 rounded-ss-xl rounded-ee-xl font-medium text-[18px] border-2 border-t-green-400  borderc   border-e-blue-500   text-color">
         {" "}
-        Join Us Now
+        <span className="  text-color-v3">Join Us Now</span>
       </button>
     </div>
   );
